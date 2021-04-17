a259a8bd00c897baf0d020bc2effc29c26fd6b72
josuescardini <>
2021-04-17 14:56:21 +0000

    Numero 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  int num1, num2,j, sobra, novoValor = 0, novoValor2=0;
+  int somatorio, sub, multi, div, rest;
+  char operacao;
+
+  //numero 1
+  cout<< "Digite um número binário entre 0 e 11111111 "<<endl;
+  cin>> num1;
+  
+  //Transformando para decimal
+   for (j = 0; j < 6; j++) {
+        sobra = num1 - ((num1 / 10)*10);
+        num1 = num1 / 10;
+        novoValor += sobra * pow(2,j);
+    }
+
+  //numero 2
+  
+
+
+
+
+
+
 }
